5d7bdfb0b3c832f509819869ee25ede38a286de8
Tomato1937 <student@pisem.net>
2022-06-10 17:18:54 +0700

    Homework_2_1

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,11 +1,57 @@
- package ru.netology.nmedia
+package ru.netology.nmedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import ru.netology.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            published = "21 мая в 18:36",
+            likedByMe = false
+        )
+        with(binding) {
+            tvAuthor.text = post.author
+            tvPublished.text = post.published
+            tvContent.text = post.content
+            if (post.likedByMe) {
+                ibLike?.setImageResource(R.drawable.ic_baseline_favorited_border_24)
+            }
+            tvLike?.text = post.likes.toString()
+            tvShare?.text = post.shares.toString()
+
+            root.setOnClickListener {
+                Log.d("stuff", "stuff")
+            }
+
+            ivAvatar.setOnClickListener {
+                Log.d("stuff", "avatar")
+            }
+
+            ibLike?.setOnClickListener {
+                Log.d("stuff", "like")
+                post.likedByMe = !post.likedByMe
+                ibLike.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_baseline_favorited_border_24
+                    else R.drawable.ic_baseline_favorite_border_24
+                )
+                if (post.likedByMe) post.likes++ else post.likes--
+                tvLike.text = totalCount(post.likes)
+            }
+
+            ibShare?.setOnClickListener {
+                Log.d("stuff", "share")
+                post.shares++
+                tvShare.text = totalCount(post.shares)
+            }
+        }
     }
 }
---
+++ app/src/main/java/ru/netology/nmedia/TotalCount.kt
@@ -0,0 +1,9 @@
+package ru.netology.nmedia
+
+fun totalCount(count: Int): String {
+    return when (count) {
+        in 0..999 -> "$count"
+        in 1_000..999_999 -> "${((count / 100).toDouble() / 10)}K"
+        else -> "${((count / 100_000).toDouble() / 10)}M"
+    }
+}
